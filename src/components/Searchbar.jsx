c4b6290949e34969a12b3205a71629b473f4bbc5
Ivan Caldarella <gamein3x@gmail.com>
2026-05-19 12:43:36 +0200

    Progresso con la fetch, manca autorizzazione

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -9,8 +9,9 @@ import styles from '../styles/Searchbar.module.css';
 function Searchbar() {
     const [query, setQuery] = useState('');
     const [loading, setLoading] = useState(true);
+    const [results, setResults] = useState([]);
 
-    const API_urlAndquery = `https://api.themoviedb.org/3/search/movie/${query}`;
+    const API_urlAndquery = `https://api.themoviedb.org/3/search/movie?query=${query}&language=it-IT`; //
 
     function searchAPI() {
         fetch(API_urlAndquery)
@@ -18,14 +19,30 @@ function Searchbar() {
                 if (!response.ok) {
                     throw new Error("Errore nella richiesta");
                 };
-                return response.json();
+                return response.json(); // Qui ho fatto prendere la response 
+            })
+            .then((data) => {
+                setResults(data.results);
+                console.log(results); // Se tutto funziona mi restituirà i risultati
             })
             .catch((error) => (console.log(error)))
             .finally(() => {
                 setLoading(false);
-                return console.log(loading ?"Loading completo" : "Loading fallito");
+                return console.log(loading ? "Loading completo" : "Loading non eseguito");
             });
     };
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            if (query.trim() !== "") {
+                searchAPI();
+            } else {
+                setResults([]); //
+            }
+        }, 500);
+
+        return () => clearTimeout(timeout);
+    }, [query]);
 
     const handleInputChange = (input) => {
         setQuery(input.target.value);  // aggiorna lo stato a ogni tasto
