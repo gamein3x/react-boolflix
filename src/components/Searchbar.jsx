1e705e1fbfbd9fff323b09313f343a9a6b8eef09
Ivan Caldarella <gamein3x@gmail.com>
2026-05-19 12:22:20 +0200

    Fetch aggiornata

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -6,18 +6,26 @@ import { Search } from 'react-bootstrap-icons';
 import styles from '../styles/Searchbar.module.css';
 
 
-
-function dannataFetch() {
-
-}
-
-
-
-
 function Searchbar() {
     const [query, setQuery] = useState('');
+    const [loading, setLoading] = useState(true);
 
     const API_urlAndquery = `https://api.themoviedb.org/3/search/movie/${query}`;
+
+    function searchAPI() {
+        fetch(API_urlAndquery)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Errore nella richiesta");
+                };
+                return response.json();
+            })
+            .catch((error) => (console.log(error)))
+            .finally(() => {
+                setLoading(false);
+                return console.log(loading ?"Loading completo" : "Loading fallito");
+            });
+    };
 
     const handleInputChange = (input) => {
         setQuery(input.target.value);  // aggiorna lo stato a ogni tasto
@@ -32,12 +40,7 @@ function Searchbar() {
                     placeholder=" "
                     value={query}
                     onChange={handleInputChange}
-                />
-                <label
-                    htmlFor="searchbar"
-                    className={styles.searchLabel}>
-                    Search here!
-                </label>
+                    className={styles.searchLabel} />
             </div>
             <div>
                 <button className="btn">
